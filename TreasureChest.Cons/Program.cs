35d8e2167722cac58f37db0df852604c29f17f7b
maximlesy <maximlesy@gmail.com>
2021-02-11 10:16:58 +0100

    Remove refs

--- TreasureChest.Cons/Program.cs
+++ TreasureChest.Cons/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using TreasureChest.Cons.Core.Domain;
-using TreasureChest.Cons.Core.Domain.Items;
-using TreasureChest.Cons.Core.Domain.Keys;
-using TreasureChest.Cons.Core.Domain.Locks;
 
 namespace TreasureChest.Cons
 {
