b87e7f5399dd25ab83fb125b79347906ac8dba7b
maximlesy <maximlesy@gmail.com>
2021-02-11 10:10:37 +0100

    Initial commit

---
+++ TreasureChest.Cons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TreasureChest.Cons.Core.Domain;
+using TreasureChest.Cons.Core.Domain.Items;
+using TreasureChest.Cons.Core.Domain.Keys;
+using TreasureChest.Cons.Core.Domain.Locks;
+
+namespace TreasureChest.Cons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ahoy! Create your treasure chests here.
+        }
+
+    }
+}
